10dde54f5d4411d7d6d5a5a0f3b31280806f7631
Xinyi Rong <Lizard.rar@gmail.com>
2021-11-16 22:35:49 -0800

    Update to support new Pixiv following page

--- src/Open 10 New Pixiv Following.js
+++ src/Open 10 New Pixiv Following.js
@@ -1,28 +1,30 @@
 javascript:(function () {
   'use strict';
-  const element = document.getElementById('js-mount-point-latest-following');
-  const storage = window.sessionStorage;
-  const maxOpen = 10;
-  if (!element) {
-    alert('Mount point not found.');
-    return;
-  }
-  try {
-    const data = JSON.parse(element.getAttribute('data-items'));
-    let openCount = 0;
-    for (const illust of data) {
-      const illustId = illust['illustId'];
-      if (storage.getItem(illustId) === null) {
-        if (openCount >= maxOpen) {
-          break;
+  const jqueryTag = document.createElement('script');
+  jqueryTag.src = '//code.jquery.com/jquery-latest.min.js';
+  jqueryTag.onload = function () {
+    const $ = jQuery.noConflict();
+    const storage = window.sessionStorage;
+    const maxOpen = 10;
+    try {
+      const ids = $('ul li div[type=illust] a')
+        .map((i, e) => $(e).data('gtm-value').toString())
+        .toArray();
+      let openCount = 0;
+      for (const illustId of ids) {
+        if (storage.getItem(illustId) === null) {
+          if (openCount >= maxOpen) {
+            break;
+          }
+          storage.setItem(illustId, (new Date()).toISOString());
+          ++openCount;
+          window.open(`https://www.pixiv.net/artworks/${illustId}`, '_blank');
         }
-        storage.setItem(illustId, (new Date()).toISOString());
-        ++openCount;
-        window.open(`https://www.pixiv.net/artworks/${illustId}`, '_blank');
       }
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
     }
-  } catch (error) {
-    console.error(error);
-    alert(error.message);
-  }
+  };
+  document.head.appendChild(jqueryTag);
 })();
