2d590ace5844b69fd73970b06dd6f1142d957aa7
Xinyi Rong <Lizard.rar@gmail.com>
2024-06-01 09:26:21 +0800

    Rename Twitter to X

--- src/Go to X.js
+++ src/Go to X.js
@@ -1,7 +1,7 @@
 javascript:(function () {
   'use strict';
-  let user = prompt('Twitter User');
+  let user = prompt('X User');
   if (user && (user = user.trim())) {
-    window.open(`https://twitter.com/${user}`);
+    window.open(`https://x.com/${user}`);
   }
 })();
