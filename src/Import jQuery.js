306979013cf62771b8d0f1cb6671f4662d5eebda
Xinyi Rong <Lizard.rar@gmail.com>
2024-08-14 20:05:13 -0700

    Override CSP policy

--- src/Import jQuery.js
+++ src/Import jQuery.js
@@ -1,8 +1,17 @@
 javascript: (function (e, s) {
+  if (window.trustedTypes && window.trustedTypes.createPolicy) {
+    if (window.trustedTypes.defaultPolicy === null) {
+      window.trustedTypes.createPolicy('default', {
+        createHTML: string => string,
+        createScriptURL: string => string,
+        createScript: string => string,
+      });
+    }
+  }
   e.src = s;
   e.onload = function () {
     jQuery.noConflict();
     console.log('jQuery injected');
   };
   document.head.appendChild(e);
-})(document.createElement('script'), '//code.jquery.com/jquery-3.6.0.min.js')
+})(document.createElement('script'), '//code.jquery.com/jquery-4.0.0-beta.min.js')
